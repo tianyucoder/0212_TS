1884c95cf6dd0b0218d0a036d45b912302f56fef
tianyu5177 <zhangtianyu5177@sina.com>
2020-08-01 11:38:20 +0800

    更新

---
+++ src/01_hello_ts/hello.ts
@@ -0,0 +1,9 @@
+(()=>{
+
+	function welcome(name:string):number{
+		console.log(`欢迎${name}先生/女士，来到TS的世界!!!!!`);
+		return 1
+	}
+	welcome('tom')
+
+})()
---
+++ src/main.ts
@@ -0,0 +1,1 @@
+import './01_hello_ts/hello'
